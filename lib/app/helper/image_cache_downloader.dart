e6d70cec931f2c84501e5769e8f6d9f63f112d6e
Mark Dionnie <markb@globalwebforce.com>
2023-07-19 13:01:05 +0800

    up

--- lib/app/helper/image_cache_downloader.dart
+++ lib/app/helper/image_cache_downloader.dart
@@ -1,11 +1,15 @@
+import 'package:digicard/app/extensions/string_extension.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 Future<Uint8List?> getNetworkImageData(String url) async {
   try {
-    final file = await DefaultCacheManager().getSingleFile(url, key: url);
-    final bytes = await file.readAsBytes();
-    return Uint8List.fromList(bytes);
+    if (url.isValidUrl()) {
+      final file = await DefaultCacheManager().getSingleFile(url, key: url);
+      final bytes = await file.readAsBytes();
+      return Uint8List.fromList(bytes);
+    }
+    return null;
   } catch (e) {
     return null;
   }
