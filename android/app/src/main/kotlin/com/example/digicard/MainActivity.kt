afadbfb5c05e0cca32c35153c85cd180c8c97151
markbulingit <markb@globalwebforce.com>
2023-11-28 17:53:21 +0800

    up

--- android/app/src/main/kotlin/com/example/digicard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.digicard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/app/extensions/contacts_service_extension.dart
+++
@@ -1,8 +0,0 @@
-import 'package:digicard/app/services/contacts_service.dart';
-
-extension ContactsServiceExtension on ContactsService {
-  bool isExist({required int? id}) {
-    final temp = contacts.indexWhere((e) => e.id == id);
-    return temp != -1 ? true : false;
-  }
-}
